8a46451ccd1d82c1c2c6e323849ac66458c4e217
PasNA6713 <diementros@gmail.com>
2022-02-14 19:51:34 +0300

    use inspect.get_annotations to type applying

--- propan/annotations/decorate.py
+++ propan/annotations/decorate.py
@@ -1,19 +1,28 @@
 from functools import wraps
+from inspect import get_annotations
 
 
 def apply_types(func):
-    types = func.__annotations__
+    types = get_annotations(func)
+    ret_type = types.pop('return', None)
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        args_position = 0
         kw = {}
-        for arg, arg_type in types.items():
-            kw[arg] = kwargs.pop(arg, None)
-            if kw[arg] is None:
-                kw[arg] = args[args_position]
-                args_position += 1
-            if not isinstance(kw[arg], arg_type):
-                kw[arg] = arg_type(kw[arg])
-        return func(**kw)
+
+        for (arg_name, arg_type), arg in zip(types.items(), args):
+            if not isinstance(arg, arg_type):
+                arg = arg_type(arg)
+            kw[arg_name] = arg
+        
+        for arg_name, arg in kwargs.items():
+            arg_type = types[arg_name]
+            if not isinstance(arg, arg_type):
+                arg = arg_type(arg)
+            kw[arg_name] = arg
+        
+        if ret_type is None:
+            return func(**kw)
+        else:
+            return ret_type(func(**kw))
     return wrapper
