24e26617dc7abb305dbe362212f3a27128524402
Lancetnik <diementros@yandex.ru>
2023-07-10 18:26:48 +0300

    add kafka publish_batch test

--- tests/brokers/kafka/test_publish.py
+++ tests/brokers/kafka/test_publish.py
@@ -1,9 +1,33 @@
+import asyncio
+
 import pytest
 
+from propan import KafkaBroker
 from tests.brokers.base.publish import BrokerPublishTestcase
 
 
 @pytest.mark.kafka
 @pytest.mark.slow
 class TestKafkaPublish(BrokerPublishTestcase):
-    pass
+    @pytest.mark.asyncio
+    async def test_publish_batch(self, queue: str, broker: KafkaBroker):
+        msgs_queue = asyncio.Queue(maxsize=2)
+
+        @broker.handle(queue)
+        async def handler(msg):
+            await msgs_queue.put(msg)
+
+        async with broker:
+            await broker.start()
+
+            await broker.publish_batch(1, "hi", topic=queue)
+
+            result, _ = await asyncio.wait(
+                (
+                    asyncio.create_task(msgs_queue.get()),
+                    asyncio.create_task(msgs_queue.get()),
+                ),
+                timeout=3,
+            )
+
+        assert {1, "hi"} == {r.result() for r in result}
