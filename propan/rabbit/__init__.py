4f461fe4e4f542f9276a75929a8ddda6089abe7f
Lancetnik <diementros@yandex.ru>
2023-07-28 17:54:58 +0300

    docs: add middlewares usage example

--- propan/rabbit/__init__.py
+++ propan/rabbit/__init__.py
@@ -1,5 +1,6 @@
 from propan.rabbit.broker import RabbitBroker
 from propan.rabbit.router import RabbitRouter
+from propan.rabbit.message import RabbitMessage
 from propan.rabbit.shared.constants import ExchangeType
 from propan.rabbit.shared.schemas import RabbitExchange, RabbitQueue
 from propan.rabbit.test import TestRabbitBroker
@@ -11,4 +12,5 @@ __all__ = (
     "RabbitQueue",
     "ExchangeType",
     "RabbitRouter",
+    "RabbitMessage",
 )
