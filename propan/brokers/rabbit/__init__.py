8ab7fe3de8c7d9483c2f9a0c4549abe3580cc3db
Gerardo Tatzati <gerardotatzati@gmail.com>
2023-07-31 14:39:55 -0400

    Update __init__.py

--- propan/brokers/rabbit/__init__.py
+++ propan/brokers/rabbit/__init__.py
@@ -1,6 +1,7 @@
 from propan.brokers.rabbit.rabbit_broker import RabbitBroker, RabbitMessage
 from propan.brokers.rabbit.routing import RabbitRouter
 from propan.brokers.rabbit.schemas import ExchangeType, RabbitExchange, RabbitQueue
+from propan.brokers.rabbit.rabbit_broker_sync import RabbitSyncBroker
 
 __all__ = (
     "RabbitBroker",
@@ -9,4 +10,5 @@ __all__ = (
     "RabbitExchange",
     "ExchangeType",
     "RabbitMessage",
+    "RabbitSyncBroker",
 )
